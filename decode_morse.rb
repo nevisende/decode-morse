f9b0d37858e5e87b915ff129d8b04d23dd5df8f7
Chimwemwe-127001 <sinyinzachimwemwe@gmail.com>
2022-02-23 15:02:09 +0200

    Create a method to decode the entire message in Morse code

--- decode_morse.rb
+++ decode_morse.rb
@@ -11,3 +11,29 @@ end
 
 puts decode_char('.-')
 # A
+
+def decode_word(word)
+  decoded_word = []
+  char = word.split
+  char.each do |i|
+    decoded_word << decode_char(i)
+  end
+  decoded_word = decoded_word.join
+end
+
+puts decode_word('-- -.--')
+# MY
+
+def decode(message)
+  decoded_message = []
+  word = message.split
+  word.each do |w|
+    decoded_message << decode_word(w)
+  end
+  puts decoded_message = decoded_message.join(' ')
+end
+
+decode('-- -.-- -. .- -- .')
+# MY NAME
+decode('.- -... --- -..- ..-. ..- .-.. .-.. --- ..-. .-. ..- -... .. . ... ')
+# A BOX FULL OF RUBIES
